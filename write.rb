9b30a34f7ca6ecd3b167754afdbb8c390cfb1b16
Ian Hunter <beanland2@gmail.com>
2014-04-07 14:55:25 -0600

    Initial commit.

---
+++ write.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+CONSONTANTS = {
+  'p'=>'m',
+  'b'=>'b',
+  'g'=>'g',
+  'd'=>'d',
+  'ɖ'=>'ɖ',
+  'l'=>'l',
+  't'=>'l',
+  'ɣ'=>'k',
+  'k'=>'k',
+  'ʈ'=>'ʈ'
+  'ɭ'=>'ʈ'
+}
+
+VOWELS = 'aioue'.split('')
+
+
